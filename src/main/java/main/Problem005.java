d8cb2b5f556942843fe9c2c19661c8805589c05b
saurabhk <saurabhk@a-t-g.com>
2012-02-01 16:20:25 -0600

    5th problem solved, not perfect answer

--- src/main/java/main/Problem005.java
+++ src/main/java/main/Problem005.java
@@ -9,7 +9,56 @@ package main;
 public class Problem005 {
 
 	public static void main(String[] args) {
+
+		int number = 1;
+		int smallestNumber = 0;
 		
+		while (number <= 1000000000) {
+			
+			if (number % 1 != 0){ number++; continue; }
+			
+			if (number % 2 != 0){ number++; continue; }
+			
+			if (number % 3 != 0){ number++; continue; }
+			
+			if (number % 4 != 0){ number++; continue; }
+			
+			if (number % 5 != 0){ number++; continue; }
+			
+			if (number % 6 != 0){ number++; continue; }
+			
+			if (number % 7 != 0){ number++; continue; }
+			
+			if (number % 8 != 0){ number++; continue; }
+			
+			if (number % 9 != 0){ number++; continue; }
+			
+			if (number % 10 != 0){ number++; continue; }
+			
+			if (number % 11 != 0){ number++; continue; }
+			
+			if (number % 12 != 0){ number++; continue; }
+			
+			if (number % 13 != 0){ number++; continue; }
+			
+			if (number % 14 != 0){ number++; continue; }
+			
+			if (number % 15 != 0){ number++; continue; }
+			
+			if (number % 16 != 0){ number++; continue; }
+			
+			if (number % 17 != 0){ number++; continue; }
+			
+			if (number % 18 != 0){ number++; continue; }
+			
+			if (number % 19 != 0){ number++; continue; }
+			
+			if (number % 20 != 0){ number++; continue; }			
+			
+			smallestNumber = number;
+			break;
+		}
+		System.out.println("smallest Number: "+ smallestNumber);
 
 	}
 
